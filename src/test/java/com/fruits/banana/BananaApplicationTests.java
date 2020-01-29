5499a1425bb4189ae5cff50081ae703b9e19440d
huhuhahaka <1739660721@qq.com>
2020-01-29 19:45:05 +0800

    项目初建

---
+++ src/test/java/com/fruits/banana/BananaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fruits.banana;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BananaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
